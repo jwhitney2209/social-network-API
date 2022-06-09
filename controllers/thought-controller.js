d52e331040f50f9fae6a27f9363e5ca73c8b995c
teachergg <jwhitney@bisd.us>
2022-06-09 01:04:25 -0500

    user routes finished

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,5 +1,45 @@
 const { Thought, User } = require('../models');
 
 const thoughtController = {
-  
-}
+  // get all thoughts
+  getAllThought(req, res) {
+    Thought.find({})
+      .then(dbThoughtData => res.json(dbThoughtData))
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(400);
+      })
+  },
+  // get one thought by id 
+  getThoughtById({ params }, res) {
+    Thought.findOne({ _id: params.id })
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: 'No thought found with this id!' });
+          return;
+        }
+        res.json(dbThoughtData)
+      })
+      .catch(err => {
+        console.log(err);
+        res.sendStatus(400);
+      });
+  },
+  // create thought
+  createThought({ body }, res) {
+    Thought.create(body)
+    .then((dbThoughtData) => {
+      return User.findOneAndUpdate(
+        { _id: body.userId },
+        { $push: { thoughts: thoughtData._id } },
+        { new: true }
+      ) // write then/catch
+    })
+  },
+  updateThought() {},
+  deleteThought() {},
+  addReaction() {},
+  deleteReaction() {}
+};
+
+module.exports = thoughtController;
